bba12818d602eacdf8f7f11ad26b75511326c562
Surfincolin <colinwageman@gmail.com>
2020-12-13 18:54:05 -0800

    initial commit, working SVM with Adult data

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 19:54:26 2020
+
+@author: colinwageman
+"""
+
+# SVMs
+# Naive Bayes
+# Random Forests
+
+# ADULTS - https://archive.ics.uci.edu/ml/datasets/Adult
+# Covertype - https://archive.ics.uci.edu/ml/datasets/Covertype
+# EEG Eye State Data Set - https://archive.ics.uci.edu/ml/datasets/EEG+Eye+State
+# MoCap Hand Postures Data Set - https://archive.ics.uci.edu/ml/datasets/MoCap+Hand+Postures#
